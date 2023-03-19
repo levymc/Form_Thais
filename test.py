8c9da5ff90e1afe3befa96ba090e9572fb6cd032
levymc <levymcruz@gmail.com>
2023-03-19 14:31:16 -0300

    fianl config

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
 import random
 
-mapQuest2 = ['i34', 'i37', 'i40']
-posicaoQuest2 = [0, 1, 2]
-pesosQuest2 = [1, 2, 1]
-aleatorioQuest2 = random.choices(posicaoQuest2, pesosQuest2, k=1)[0]
-
-print(mapQuest2[aleatorioQuest2])
+print(random.choices([1, 2], [2, 1], k=1)[0])
